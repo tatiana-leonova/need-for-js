0e48d4690f284cf46c16111bdf63018229a19af8
Tatiana <lts221@yandex.ru>
2020-09-10 18:16:03 +0300

    ДЗ 3

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ function getQuantityElements(heightElement) {
 
 function startGame() {
   start.classList.add("hide");
+  gameArea.innerHTML = "";
 
   for (let i = 0; i < getQuantityElements(100); i++) {
     const line = document.createElement("div");
@@ -50,8 +51,12 @@ function startGame() {
     gameArea.appendChild(enemy);
   }
 
+  setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
+  car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2 + "px";
+  car.style.top = "auto";
+  car.style.bottom = "10px";
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
@@ -59,6 +64,9 @@ function startGame() {
 
 function playGame() {
   if (setting.start) {
+    setting.score += setting.speed;
+    score.innerHTML = "SCORE<br>" + setting.score;
+
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0) {
@@ -110,7 +118,22 @@ function moveRoad() {
 
 function moveEnemy() {
   let enemy = document.querySelectorAll(".enemy");
+
   enemy.forEach(function (item) {
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+    if (
+      carRect.top <= enemyRect.bottom &&
+      carRect.right >= enemyRect.left &&
+      carRect.left <= enemyRect.right &&
+      carRect.bottom >= enemyRect.top
+    ) {
+      setting.start = false;
+      console.warn("DTP");
+      start.classList.remove("hide");
+      start.style.top = score.offsetHeight + "px";
+    }
     item.y += setting.speed / 2;
     item.style.top = item.y + "px";
 
